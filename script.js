19d8eee3d48ed3514c3681108b0918f248135644
Tamim Ahmed <tamim20072@gmail.com>
2024-09-19 13:47:56 +0600

    second commit

--- script.js
+++ script.js
@@ -10,5 +10,16 @@ document.getElementById('login-btn').addEventListener('click', function (event) 
 
     // step - 3: get the phone number
     const phoneNumber = document.getElementById('phone-number').value
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+
+    console.log(phoneNumber, pinNumber);
+
+    // this is temporary. you should do like this
+    if (phoneNumber === '5' && pinNumber === '1234'){
+        console.log('Your are logged in ');
+        window.location.href = './home.html'
+    }
+    else {
+        alert('Wrong phone number and pin');
+    }
 })
